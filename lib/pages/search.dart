2a39d9080d99351422ad205cd71dfb710a83ba0b
SandalEmre <sandalemre5855@gmail.com>
2024-02-21 11:19:53 +0300

    mjjsjsd

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatefulWidget {
-  const SearchPage({super.key});
+  const SearchPage({Key? key});
 
   @override
   State<SearchPage> createState() => _SearchPage();
@@ -10,15 +10,53 @@ class SearchPage extends StatefulWidget {
 class _SearchPage extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Center(
+            child: IconButton(
+              icon: Icon(Icons.directions_bus),
+              onPressed: () {
+                // Otobüs seçeneğine tıklandığında yapılacak işlemler
+              },
+            ),
+          ),
+          Center(
+            child: IconButton(
+              icon: Icon(Icons.airplanemode_active),
+              onPressed: () {
+                // Uçak seçeneğine tıklandığında yapılacak işlemler
+              },
+            ),
+          ),
+          Center(
+            child: IconButton(
+              icon: Icon(Icons.hotel),
+              onPressed: () {
+                // Otel seçeneğine tıklandığında yapılacak işlemler
+              },
+            ),
+          ),
+          Center(
+            child: IconButton(
+              icon: Icon(Icons.directions_car),
+              onPressed: () {
+                // Araç seçeneğine tıklandığında yapılacak işlemler
+              },
+            ),
+          ),
+          Center(
+            child: IconButton(
+              icon: Icon(Icons.directions_boat),
+              onPressed: () {
+                // Feribot seçeneğine tıklandığında yapılacak işlemler
+              },
+            ),
+          ),
+        ],
+      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Emirhan Akkuş"),
-            Text("Emre Sandal"),
-          ],
-        ),
+        child: Text('Select your travel option here'),
       ),
     );
   }
