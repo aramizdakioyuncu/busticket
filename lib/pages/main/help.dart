f72fcd08d8ecb73a294ac11894457d8946989eb0
Mustafa Akgün <ma330007@gmail.com>
2024-03-04 18:36:50 +0300

    Yardım sayfası baştan yapıldı ve düzenlendi.

--- lib/pages/main/help.dart
+++ lib/pages/main/help.dart
@@ -25,16 +25,84 @@ class _HelpPage extends State<HelpPage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 239, 232, 232),
         appBar: AppBar(
-          title: Text(
+          title: const Text(
             "Yardım",
             style: TextStyle(color: Colors.white),
           ),
           centerTitle: true,
           backgroundColor: Colors.red,
         ),
-        body: Column(
-          children: [],
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              // Padding(
+              //   padding: const EdgeInsets.all(10.0),
+              //   child: Container(
+              //     color: Colors.red,
+              //     child: TextField(
+              //       decoration: InputDecoration(
+              //         hintText: "Nasıl Yardımcı Olabiliriz?",
+              //         border: InputBorder(
+              //           borderSide: BorderSide(color: Colors.green),
+              //         ),
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: ListTile(
+                  tileColor: Colors.white,
+                  leading: Icon(Icons.directions_bus_sharp),
+                  title: Text("Otobüs Bileti"),
+                  subtitle: Text("Sıkça Sorulan Sorular"),
+                  trailing: Icon(Icons.keyboard_arrow_right_rounded),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: ListTile(
+                  tileColor: Colors.white,
+                  leading: Icon(Icons.flight),
+                  title: Text("Uçak Bileti"),
+                  subtitle: Text("Sıkça Sorulan Sorular"),
+                  trailing: Icon(Icons.keyboard_arrow_right_rounded),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: ListTile(
+                  tileColor: Colors.white,
+                  leading: Icon(Icons.hotel),
+                  title: Text("Otel"),
+                  subtitle: Text("Sıkça Sorulan Sorular"),
+                  trailing: Icon(Icons.keyboard_arrow_right_rounded),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: ListTile(
+                  tileColor: Colors.white,
+                  leading: Icon(Icons.directions_car_rounded),
+                  title: Text("Araç Kiralama"),
+                  subtitle: Text("Sıkça Sorulan Sorular"),
+                  trailing: Icon(Icons.keyboard_arrow_right_rounded),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: ListTile(
+                  tileColor: Colors.white,
+                  leading: Icon(Icons.directions_boat_rounded),
+                  title: Text("Feribot Bileti"),
+                  subtitle: Text("Sıkça Sorulan Sorular"),
+                  trailing: Icon(Icons.keyboard_arrow_right_rounded),
+                ),
+              ),
+            ],
+          ),
         ));
   }
 }
