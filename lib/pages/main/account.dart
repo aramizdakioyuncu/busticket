1a225b19a3cfb16f376af0e3171ad92800814341
Oğuzhan Bayırlı <oguzhanbayirli7@gmail.com>
2024-03-05 17:11:32 +0300

    hesabımmm

--- lib/pages/main/account.dart
+++ lib/pages/main/account.dart
@@ -22,6 +22,62 @@ class _AccountPage extends State<AccountPage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        centerTitle: true,
+        title: Text(
+          "Hesabım",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: const Column(
+        children: [
+          ListTile(
+            title: Text("Seyahatlerim"),
+            leading: Icon(Icons.roundabout_left),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+          ListTile(
+            title: Text("Kampanyalarım"),
+            leading: Icon(Icons.card_giftcard),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+          ListTile(
+            title: Text("Alarmlarım"),
+            leading: Icon(Icons.alarm),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+          ListTile(
+            title: Text("Profilim"),
+            leading: Icon(Icons.man),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+          ListTile(
+            title: Text("Yardım"),
+            leading: Icon(Icons.help),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+          ListTile(
+            title: Text("Ayarlar"),
+            leading: Icon(Icons.settings),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+          ListTile(
+            title: Text("Hakkımızda"),
+            leading: Icon(Icons.info),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ListTile(
+            title: Text("Çıkış Yap"),
+            leading: Icon(Icons.exit_to_app),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+        ],
+      ),
+    );
   }
 }
